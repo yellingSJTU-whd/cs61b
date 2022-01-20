4a60615b463fbdb4f575e2079ad02b01cefc7cd3
10300749 <wang.haode@zte.com.cn>
2022-01-20 16:51:10 +0800

    cs61b proj1a adding ArrayDeque and LinkedListDeque skeleton

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,97 @@
+public class ArrayDeque<T> {
+
+    /**
+     * Creates an empty deque.
+     */
+    public ArrayDeque() {
+        size = 0;
+        front = -1;
+        rear = 0;
+        items = (T[]) new Object[DEFAULT_SIZE];
+    }
+
+    /**
+     * Creates a deep copy of other deque.
+     *
+     * @param other the ArrayDeque to be copied
+     */
+    public ArrayDeque(ArrayDeque other) {
+    }
+
+    /**
+     * Add an item to the head of deque.
+     *
+     * @param item the item to add
+     */
+    public void addFirst(T item) {
+        if (size == items.length) {
+            resize(items.length * RESIZE_FACTOR_UPPER);
+        }
+        if (front == -1) {
+            front = rear = 0;
+        } else if (front == 0) {
+            front = size - 1;
+        } else front--;
+        items[front] = item;
+    }
+
+    /**
+     * Add an item to the end of deque.
+     *
+     * @param item the item to add
+     */
+    public void addLast(T item) {
+        if (size == items.length) {
+            resize(items.length * RESIZE_FACTOR_UPPER);
+        }
+        rear++;
+        while (rear > size - 1) {
+            rear -= (size - 1);
+        }
+        items[rear] = item;
+    }
+
+    /**
+     * Remove the first item in deque
+     *
+     * @return null if deque is empty, the removing item otherwise
+     */
+    public T removeFirst() {
+        return null;
+    }
+
+    public T removeLast() {
+        return null;
+    }
+
+    /**
+     * Get the item at specified index from the ArrayDeque.
+     *
+     * @param index zero-based index
+     * @return null if no such item exists, the item at specified index otherwise
+     */
+    public T get(int index) {
+        return null;
+    }
+
+    /**
+     * Get the number of items in the ArrayDeque.
+     *
+     * @return size of the ArrayDeque
+     */
+    public int size() {
+        return size;
+    }
+
+    private int size;
+    private int front;
+    private int rear;
+    private T[] items;
+    private static final int RESIZE_FACTOR_UPPER = 2;
+    private static final double RESIZE_FACTOR_LOWER = 0.25;
+    private static final int DEFAULT_SIZE = 8;
+
+    private void resize(int newSize) {
+
+    }
+}
---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,171 @@
+/**
+ * Deque (usually pronounced like “deck”) is an irregular acronym of double-ended queue. Double-ended queues are
+ * sequence containers with dynamic sizes that can be expanded or contracted on both ends (either its front or its back).
+ *
+ * @param <T> type of items in the deque.
+ */
+public class LinkedListDeque<T> {
+
+    /**
+     * Constructor for LinkedListDeque.
+     */
+    public LinkedListDeque() {
+        size = 0;
+        sentinel = new Node((T) new Object(), null, null);
+        sentinel.pre = sentinel;
+        sentinel.next = sentinel;
+    }
+
+    /**
+     * Creates a deep copy of other LinkedListDeque.
+     *
+     * @param other the LinkedListDeque to be copied
+     */
+    public LinkedListDeque(LinkedListDeque<T> other) {
+        size = other.size;
+        sentinel = new Node((T) new Object(), null, null);
+
+        Node ptr = other.sentinel;
+        while (ptr.next != sentinel) {
+            ptr = ptr.next;
+            addLast(ptr.item);
+        }
+    }
+
+    /**
+     * Adds an item of type T to the head of the deque.
+     *
+     * @param item the item to be added
+     */
+    public void addFirst(T item) {
+        Node first = new Node(item, sentinel, sentinel.next);
+        sentinel.next.pre = first;
+        sentinel.next = first;
+    }
+
+    /**
+     * Adds an item of type T to the end of the deque.
+     *
+     * @param item the item to be added
+     */
+    public void addLast(T item) {
+        Node last = new Node(item, sentinel.pre, sentinel);
+        sentinel.pre.next = last;
+        sentinel.pre = last;
+    }
+
+    /**
+     * Returns true if deque is empty, false otherwise.
+     *
+     * @return true if deque is empty, false otherwise
+     */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /**
+     * Returns the number of items in the deque.
+     *
+     * @return the number of items in the deque
+     */
+    public int size() {
+        return size;
+    }
+
+    /**
+     * Prints the items in the deque from first to last, separated by a space. Once all the items have been printed,
+     * print out a new line.
+     */
+    public void printDeque() {
+        Node ptr = sentinel;
+        while (ptr.next != sentinel) {
+            ptr = ptr.next;
+            System.out.print(ptr.item + " ");
+        }
+        System.out.println();
+    }
+
+    /**
+     * Removes and returns the item at the front of the deque. If no such item exists, returns null.
+     *
+     * @return null if no such item exists, the item to be removed otherwise
+     */
+    public T removeFirst() {
+        if (sentinel.next == sentinel) {
+            return null;
+        }
+        Node first = sentinel.next;
+        first.next.pre = sentinel;
+        sentinel.next = first.next;
+        return first.item;
+    }
+
+    /**
+     * Removes and returns the item at the back of the deque. If no such item exists, returns null.
+     *
+     * @return null if no such item exists, the item to be removed otherwise
+     */
+    public T removeLast() {
+        if (sentinel.next == sentinel) {
+            return null;
+        }
+        Node last = sentinel.pre;
+        last.pre.next = sentinel;
+        sentinel.pre = last.pre;
+        return last.item;
+    }
+
+    /**
+     * Gets the item at the given index, where 0 is the front, 1 is the next item, and so forth. If no such item exists,
+     * returns null.
+     *
+     * @param index zero-based index
+     * @return null if no such item exists, the item at specified index otherwise
+     */
+    public T get(int index) {
+        if (size < index) {
+            return null;
+        }
+        Node ptr = sentinel;
+        while (index-- > 0) {
+            ptr = ptr.next;
+        }
+        return ptr.item;
+    }
+
+    /**
+     * Gets the item at the given index recursively, where 0 is the front, 1 is the next item, and so forth.
+     * If no such item exists, returns null.
+     *
+     * @param index zero-based index
+     * @return null if no such item exists, the item at specified index otherwise
+     */
+    public T getRecursive(int index) {
+        if (index < size) {
+            return null;
+        }
+        return getRecursiveHelper(index, sentinel.next);
+    }
+
+    private T getRecursiveHelper(int index, Node curr) {
+        if (index == 0) {
+            return curr.item;
+        }
+        return getRecursiveHelper(index - 1, curr.next);
+    }
+
+    private int size;
+    private Node sentinel;
+
+    private class Node {
+        private T item;
+        private Node pre;
+        private Node next;
+
+        public Node(T item, Node pre, Node next) {
+            this.item = item;
+            this.pre = pre;
+            this.next = next;
+        }
+    }
+}
