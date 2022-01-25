abbca842ec7b6a58c7a40a698aff1fd217957f02
10300749 <wang.haode@zte.com.cn>
2022-01-25 16:46:56 +0800

    cs61b proj1 gold add randomized test

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -17,13 +17,12 @@ public class TestArrayDequeGold {
     @Test
     public void randomizedTest() {
         for (int i = 0; i < 500; i++) {
-            int random = StdRandom.uniform(4);
+            int random = StdRandom.uniform(5);
             switch (random) {
                 case 0:
                     sad.addFirst(i);
                     ads.addFirst(i);
                     sb.append("addFirst(").append(i).append(")\n");
-                    assertEquals(sb.toString(), ads.size(), sad.size());
                     assertEquals(sb.toString(), ads.get(0), sad.get(0));
                     break;
 
@@ -31,30 +30,30 @@ public class TestArrayDequeGold {
                     sad.addLast(i);
                     sad.addLast(i);
                     sb.append("addLast(").append(i).append(")\n");
-                    assertEquals(sb.toString(), ads.size(), sad.size());
                     assertEquals(sb.toString(), ads.get(ads.size() - 1), sad.get(sad.size() - 1));
                     break;
 
                 case 2:
-                    assertEquals(sb.toString(), ads.size(), sad.size());
-                    if (ads.size() > 0) {
+                    if (ads.size() > 0 && sad.size() > 0) {
                         Integer student = sad.removeFirst();
                         Integer ref = ads.removeFirst();
                         sb.append("removeFirst()\n");
-                        assertEquals(sb.toString(), ads.size(), sad.size());
                         assertEquals(sb.toString(), ref, student);
                     }
                     break;
 
                 case 3:
-                    assertEquals(sb.toString(), ads.size(), sad.size());
-                    if (ads.size() > 0) {
+                    if (ads.size() > 0 && sad.size() > 0) {
                         Integer student = sad.removeLast();
                         Integer ref = ads.removeLast();
                         sb.append("removeLast()\n");
-                        assertEquals(sb.toString(), ads.size(), sad.size());
                         assertEquals(sb.toString(), ref, student);
                     }
+                    break;
+
+                case 4:
+                    sb.append("size()\n");
+                    assertEquals(sb.toString(), ads.size(), sad.size());
                     break;
             }
         }
