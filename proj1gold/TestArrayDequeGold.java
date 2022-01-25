7d13a658cc9ed499c452464a744438f475d4a334
10300749 <wang.haode@zte.com.cn>
2022-01-25 17:24:43 +0800

    cs61b proj1 gold add randomized test

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -2,6 +2,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class TestArrayDequeGold {
     StudentArrayDeque<Integer> sad;
@@ -17,44 +18,71 @@ public class TestArrayDequeGold {
     @Test
     public void randomizedTest() {
         for (int i = 0; i < 500; i++) {
-            int random = StdRandom.uniform(5);
+            int random = StdRandom.uniform(4);
             switch (random) {
                 case 0:
+                    sb.append("size()\n");
+                    assertEquals(sb.toString(), ads.size(), sad.size());
+
                     sad.addFirst(i);
                     ads.addFirst(i);
                     sb.append("addFirst(").append(i).append(")\n");
                     assertEquals(sb.toString(), ads.get(0), sad.get(0));
+
+                    sb.append("size()\n");
+                    assertEquals(sb.toString(), ads.size(), sad.size());
                     break;
 
                 case 1:
+                    sb.append("size()\n");
+                    assertEquals(sb.toString(), ads.size(), sad.size());
+
                     sad.addLast(i);
                     sad.addLast(i);
                     sb.append("addLast(").append(i).append(")\n");
                     assertEquals(sb.toString(), ads.get(ads.size() - 1), sad.get(sad.size() - 1));
+
+                    sb.append("size()\n");
+                    assertEquals(sb.toString(), ads.size(), sad.size());
                     break;
 
                 case 2:
-                    if (ads.size() > 0 && sad.size() > 0) {
+                    sb.append("size()\n");
+                    assertEquals(sb.toString(), ads.size(), sad.size());
+
+                    if (ads.isEmpty()) {
+                        sb.append("isEmpty()\n");
+                        assertTrue(sb.toString(), sad.isEmpty());
+                    } else {
                         Integer student = sad.removeFirst();
                         Integer ref = ads.removeFirst();
                         sb.append("removeFirst()\n");
                         assertEquals(sb.toString(), ref, student);
+
+                        sb.append("size()\n");
+                        assertEquals(sb.toString(), ads.size(), sad.size());
                     }
                     break;
 
                 case 3:
-                    if (ads.size() > 0 && sad.size() > 0) {
+                    sb.append("size()\n");
+                    assertEquals(sb.toString(), ads.size(), sad.size());
+
+                    if (ads.isEmpty()) {
+                        sb.append("isEmpty()\n");
+                        assertTrue(sb.toString(), sad.isEmpty());
+                    } else {
                         Integer student = sad.removeLast();
                         Integer ref = ads.removeLast();
                         sb.append("removeLast()\n");
                         assertEquals(sb.toString(), ref, student);
+
+                        sb.append("size()\n");
+                        assertEquals(sb.toString(), ads.size(), sad.size());
                     }
                     break;
 
-                case 4:
-                    sb.append("size()\n");
-                    assertEquals(sb.toString(), ads.size(), sad.size());
-                    break;
+                default:
             }
         }
     }
