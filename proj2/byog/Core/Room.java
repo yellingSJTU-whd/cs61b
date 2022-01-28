20b12f1cf9c5b0474160d7212fd9c0abc8fc4384
10300749 <wang.haode@zte.com.cn>
2022-01-28 17:26:24 +0800

    cs61b proj2 foundation

--- proj2/byog/Core/Room.java
+++ proj2/byog/Core/Room.java
@@ -19,12 +19,12 @@ public class Room {
         int height = theWorld.length;
         int width = theWorld[0].length;
 
-        int x0 = RandomUtils.uniform(random, 0, width - CAP - 1);
-        int y0 = RandomUtils.uniform(random, 0, height - CAP - 1);
+        int x0 = RandomUtils.uniform(random, 0, width - CAP);
+        int y0 = RandomUtils.uniform(random, 0, height - CAP);
         Position p0 = new Position(x0, y0);
 
-        int x1 = RandomUtils.uniform(random, x0, x0 + CAP);
-        int y1 = RandomUtils.uniform(random, y0, y0 + CAP);
+        int x1 = RandomUtils.uniform(random, x0 + 1, x0 + CAP + 1);
+        int y1 = RandomUtils.uniform(random, y0 + 1, y0 + CAP + 1);
         Position p1 = new Position(x1, y1);
 
         return new Room(p0, p1);
