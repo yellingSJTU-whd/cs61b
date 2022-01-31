304c02651cce954fe1d6681b6d123400e79a837d
yellingSJTU <haode_wang@sjtu.edu.cn>
2022-01-31 21:47:19 +0800

    ca61b proj2 world generation testing

---
+++ proj2/byog/Core/RoomTest.java
@@ -0,0 +1,14 @@
+package byog.Core;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoomTest {
+
+    @org.junit.jupiter.api.Test
+    void overLap() {
+        Room r1 = new Room(new Position(0,0), new Position(10,10));
+        Room r2 = new Room(new Position(9,9), new Position(15,15));
+
+        assertTrue(r1.overLap(r2));
+    }
+}
