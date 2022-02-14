09537f9d51b2566523a7cc6997f1419cb78d5c70
10300749 <wang.haode@zte.com.cn>
2022-02-14 16:36:12 +0800

    cs61b hw2 implement Percolation, add PercolationStats skeleton

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -2,6 +2,144 @@ package hw2;
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class Percolation {
 
+    private final WeightedQuickUnionUF weightedQuickUnionUF;
+    private final int order;
+    private int openSitesCount;
+    private final boolean[] opened;
+
+    /**
+     * Constructor for Percolation.
+     *
+     * @param N dimension of the percolation system, throws illegalArgumentException if N<=0
+     */
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new IllegalArgumentException("should be positive: " + N);
+        }
+
+        order = N;
+        openSitesCount = 0;
+        opened = new boolean[order * order];
+        Arrays.fill(opened, false);
+
+        weightedQuickUnionUF = new WeightedQuickUnionUF(order * order + 2);
+        for (int i = 0; i < order; i++) {
+            weightedQuickUnionUF.union(i, order * order);
+        }
+    }
+
+    /**
+     * Open the specified site if no already opened.
+     *
+     * @param row row of the site
+     * @param col column of the site
+     */
+    public void open(int row, int col) {
+        if (outOfRange(row) || outOfRange(col)) {
+            throw new IndexOutOfBoundsException("Order: " + order
+                    + " row: " + row + " column: " + col);
+        }
+
+        if (!isOpen(row, col)) {
+            openSitesCount++;
+            opened[convertTo1D(row, col)] = true;
+
+            int idx = convertTo1D(row, col);
+            if (row > 0 && isOpen(row - 1, col)) {
+                weightedQuickUnionUF.union(idx, convertTo1D(row - 1, col));
+            }
+            if (row < order - 1 && isOpen(row + 1, col)) {
+                weightedQuickUnionUF.union(idx, convertTo1D(row + 1, col));
+            }
+            if (col > 0 && isOpen(row, col - 1)) {
+                weightedQuickUnionUF.union(idx, convertTo1D(row, col - 1));
+            }
+            if (col < order - 1 && isOpen(row, col + 1)) {
+                weightedQuickUnionUF.union(idx, convertTo1D(row, col + 1));
+            }
+            if (col == order - 1) {
+                weightedQuickUnionUF.union(idx, order * order + 1);
+            }
+        }
+    }
+
+    private boolean outOfRange(int indices) {
+        return indices < 0 || indices >= order;
+    }
+
+    /**
+     * Return whether the specified site is open.
+     *
+     * @param row row of the site
+     * @param col column of the site
+     * @return true if open, false otherwise
+     */
+    public boolean isOpen(int row, int col) {
+        if (outOfRange(row) || outOfRange(col)) {
+            throw new IndexOutOfBoundsException("order: " + order
+                    + " row: " + row + " column: " + col);
+        }
+
+        return opened[convertTo1D(row, col)];
+    }
+
+    /**
+     * Return whether the specified site is full.
+     *
+     * @param row row of the site
+     * @param col column of the site
+     * @return true if full, false otherwise.
+     */
+    public boolean isFull(int row, int col) {
+        if (outOfRange(row) || outOfRange(col)) {
+            throw new IndexOutOfBoundsException("Order: " + order
+                    + " row: " + row + " column: " + col);
+        }
+
+        return isOpen(row, col)
+                && weightedQuickUnionUF.connected(convertTo1D(row, col), order * order);
+    }
+
+    /**
+     * Fetch the total number of open sites.
+     *
+     * @return the number of open sites
+     */
+    public int numberOfOpenSites() {
+        return openSitesCount;
+    }
+
+    /**
+     * Return whether the percolation system percolates.
+     *
+     * @return true if system percolates, false otherwise
+     */
+    public boolean percolates() {
+        return weightedQuickUnionUF.connected(order * order, order * order + 1);
+    }
+
+    public static void main(String[] args) {
+        Percolation system = new Percolation(2);
+        system.open(0, 0);
+
+        assertTrue(system.isOpen(0, 0));
+        assertFalse(system.percolates());
+
+        system.open(1, 1);
+        assertFalse(system.percolates());
+
+        system.open(1, 0);
+        assertTrue(system.percolates());
+    }
+
+    private int convertTo1D(int row, int col) {
+        return row * order + col;
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,23 @@
 package hw2;
 
 public class PercolationStats {
+    public PercolationStats(int N, int T, PercolationFactory pf) {
 
+    }
+
+    public double mean() {
+        return 0;
+    }
+
+    public double stddev() {
+        return 0;
+    }
+
+    public double confidenceLow() {
+        return 0;
+    }
+
+    public double confidenceHigh() {
+        return 0;
+    }
 }
