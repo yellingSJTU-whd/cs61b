bdf9fb4c409cfc6cd28f304c2c67cc38c38b2806
yellingSJTU <haode_wang@sjtu.edu.cn>
2022-02-26 16:18:53 +0800

    cs61b lab11 cycle detection with dfs

--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,7 +1,10 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.Stack;
+
 /**
- *  @author Josh Hug
+ * @author Josh Hug
  */
 public class MazeCycles extends MazeExplorer {
     /* Inherits public fields:
@@ -9,16 +12,54 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private final int s;
+    private final int[] pre;
 
     public MazeCycles(Maze m) {
         super(m);
+        s = StdRandom.uniform(maze.V());
+        pre = new int[maze.V()];
+        distTo[s] = 0;
     }
 
     @Override
     public void solve() {
-        // TODO: Your code here!
+        dfs(s);
     }
 
     // Helper methods go here
+    private void dfs(int v) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(v);
+
+        while (!stack.isEmpty()) {
+            int u = stack.pop();
+            marked[u] = true;
+            announce();
+
+            for (int w : maze.adj(u)) {
+                if (!marked[w]) {
+                    stack.push(w);
+                    distTo[w] = distTo[u] + 1;
+                    pre[w] = u;
+                } else if (pre[u] != w) {
+                    renderCycle(w, u);
+                    return;
+                }
+            }
+        }
+    }
+
+    private void renderCycle(int w, int u) {
+        for (int i = 0; i < maze.V(); i++) {
+            edgeTo[i] = i;
+        }
+        edgeTo[w] = u;
+        while (u != w) {
+            edgeTo[u] = pre[u];
+            u = pre[u];
+        }
+        announce();
+    }
 }
 
