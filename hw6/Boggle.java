e9659bc839c3576c830843d00196456c6a98efbe
10300749 <wang.haode@zte.com.cn>
2022-06-20 17:09:22 +0800

    cs61b hw6 efficiency

--- hw6/Boggle.java
+++ hw6/Boggle.java
@@ -12,6 +12,11 @@ public class Boggle {
 
     // File path of dictionary file
     static String dictPath = "words.txt";
+    static Trie trie;
+    static String[] board;
+    static int height;
+    static int width;
+    static Queue<Session> queue;
 
     /**
      * Solves a Boggle puzzle.
@@ -36,21 +41,20 @@ public class Boggle {
         String[] dict = in.readAllStrings();
         MinPQ<String> heap = new MinPQ<>(k, Comparator.comparingInt(String::length).
                 reversed().thenComparing(String::compareToIgnoreCase));
-        Trie trie = new Trie(dict);
-
+        trie = new Trie(dict);
         in = new In(boardFilePath);
-        String[] board = in.readAllStrings();
-        int height = board.length;
-        int width = board[0].length();
-        Queue<Session> queue = new Queue<>();
+        board = in.readAllStrings();
+        height = board.length;
+        width = board[0].length();
+        queue = new Queue<>();
 
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
                 queue.enqueue(new Session(x, y, new StringBuilder(), new boolean[height][width]));
                 while (!queue.isEmpty()) {
                     Session session = queue.dequeue();
-                    apply(session, heap, board, trie);
-                    search(queue, session, trie, board);
+                    apply(session, heap);
+                    search(session);
                 }
             }
         }
@@ -67,19 +71,17 @@ public class Boggle {
     }
 
     public static boolean[][] copyMatrix(boolean[][] original) {
-        int a = original.length;
-        int b = original[0].length;
-        boolean[][] copied = new boolean[a][b];
-        for (int i = 0; i < a; i++) {
-            copied[i] = Arrays.copyOf(original[i], b);
+        boolean[][] copy = new boolean[height][width];
+        for (int i = 0; i < height; i++) {
+            copy[i] = Arrays.copyOf(original[i], original[i].length);
         }
-        return copied;
+        return copy;
     }
 
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
         String boardPath = "smallBoard.txt";
-        List<String> solution = solve(7, boardPath);
+        List<String> solution = solve(10, boardPath);
         System.out.println(solution);
         long end = System.currentTimeMillis();
         System.out.println("time elapsed :" + (end - start));
@@ -99,7 +101,7 @@ public class Boggle {
         }
     }
 
-    private static void apply(Session session, MinPQ<String> heap, String[] board, Trie trie) {
+    private static void apply(Session session, MinPQ<String> heap) {
         int x = session.col;
         int y = session.row;
         char ch = board[y].charAt(x);
@@ -110,32 +112,31 @@ public class Boggle {
         session.visited[y][x] = true;
     }
 
-    private static void search(Queue<Session> queue, Session session, Trie trie, String[] board) {
+    private static void search(Session session) {
         boolean[][] visited = session.visited;
         StringBuilder sb = session.preSequence;
         int x = session.col;
         int y = session.row;
-        int height = board.length;
-        int width = board[0].length();
         visited[y][x] = true;
 
-        check(x > 0, visited, y, x - 1, trie, sb, board, queue);
-        check(x < width - 1, visited, y, x + 1, trie, sb, board, queue);
-        check(y > 0, visited, y - 1, x, trie, sb, board, queue);
-        check(y < height - 1, visited, y + 1, x, trie, sb, board, queue);
-        check(x > 0 && y > 0, visited, y - 1, x - 1, trie, sb, board, queue);
-        check(x > 0 && y < height - 1, visited, y + 1, x - 1, trie, sb, board, queue);
-        check(x < width - 1 && y > 0, visited, y - 1, x + 1, trie, sb, board, queue);
-        check(x < width - 1 && y < height - 1, visited, y + 1, x + 1, trie, sb, board, queue);
+        check(x > 0, visited, y, x - 1, sb);
+        check(x < width - 1, visited, y, x + 1, sb);
+        check(y > 0, visited, y - 1, x, sb);
+        check(y < height - 1, visited, y + 1, x, sb);
+        check(x > 0 && y > 0, visited, y - 1, x - 1, sb);
+        check(x > 0 && y < height - 1, visited, y + 1, x - 1, sb);
+        check(x < width - 1 && y > 0, visited, y - 1, x + 1, sb);
+        check(x < width - 1 && y < height - 1, visited, y + 1, x + 1, sb);
     }
 
-    private static void check(boolean x, boolean[][] visited, int y, int x1,
-                              Trie trie, StringBuilder sb, String[] board, Queue<Session> queue) {
-        if (x && !visited[y][x1]) {
-            if (trie.contained(sb.toString() + board[y].charAt(x1))) {
-                queue.enqueue(new Session(x1, y, new StringBuilder(sb), copyMatrix(visited)));
-            }
+    private static void check(boolean posChecker, boolean[][] visited, int row, int col,
+                              StringBuilder sb) {
+        if (!posChecker || visited[row][col]) {
+            return;
+        }
+
+        if (trie.contained(sb.toString() + board[row].charAt(col))) {
+            queue.enqueue(new Session(col, row, new StringBuilder(sb), copyMatrix(visited)));
         }
     }
-
 }
